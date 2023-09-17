f6f4f4d137010b71aa6996b2b3c85e3f95b9c252
Polichinell <tsuqinoa@gmail.com>
2023-09-17 16:40:26 +0200

    chore(project): test

---
+++ cinecoders/src/pages/actors/actorCard/Card.jsx
@@ -0,0 +1,35 @@
+import Card from "@mui/material/Card"
+import CardContent from "@mui/material/CardContent"
+import CardMedia from "@mui/material/CardMedia"
+import Typography from "@mui/material/Typography"
+import { Button, CardActionArea, CardActions } from "@mui/material"
+import Link from "next/link"
+
+export default function ActorCard(props) {
+  const { name, profile_path } = props
+
+  return (
+    <Card
+      sx={{
+        minHeight: "auto",
+        maxWidth: 300,
+        borderRadius: "20px",
+        border: "2px solid #58588a",
+      }}
+    >
+      <CardActionArea>
+        <CardMedia 
+          component="img"
+          image={"https://image.tmdb.org/t/p/w500/" + profile_path}
+          style={{ height: "200px" }}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div" sx={ { textAlign: 'center' }}>
+            {name}
+          </Typography>
+         
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  )
+}
---
+++ cinecoders/src/pages/actors/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import Grid from "@mui/material/Grid"
+import ActorCard from './actorCard/Card'
+import Navbar from '@/components/Navbar/Navbar'
+
+function Actor({actorList}) {
+
+  
+  return (
+    <div>
+      <Navbar />
+      <h1>Actor page</h1>
+    <br />
+    <Grid
+        container
+        spacing={4}
+        direction="row"
+        justifyContent="space-evenly"
+        alignItems="flex"
+      >
+        {actorList.results.map((actor) => {
+          return (
+            <Grid item xs={3} key={actor.id}>
+              <ActorCard {...actor} />
+            </Grid>
+          )
+        })}
+      </Grid>
+    
+    </div>
+  )
+}
+
+export default Actor
+
+export async function getStaticProps() {
+  const url = "https://api.themoviedb.org/3/person/popular"
+  const options = {
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZjBlYzdlNWQyNjk1NGU5Yjc3MGVhNjU0Y2EzMjk5ZiIsInN1YiI6IjY1MDBmYTYwZWZlYTdhMDBmZDFiOTUyNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8aDt5GtI-fsoy12WejvXZlqANpDVU2bfZ5gfoDmz5vw",
+    },
+  }
+
+  const response = await fetch(url, options)
+  const data = await response.json()
+
+  return {
+    props: {
+      actorList: data,
+    },
+  }
+}
